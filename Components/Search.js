03aebbfd1d8e0f9272d698df6b412b7a078e88cc
Valentin Lapointe <valentin.lapointe@viacesi.fr>
2019-09-05 08:58:38 +0200

    feat: ajout de la page de presentation et un fichier pour le score

--- Components/Search.js
+++ Components/Search.js
@@ -1,56 +1,88 @@
 import React from 'react'
-import {StyleSheet, Text, View, ScrollView, TouchableOpacity} from 'react-native'
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native'
 class Search extends React.Component{
 
   render(){
     const {navigate} = this.props.navigation;
     return(
-      <ScrollView>
-        <Text style={styles.titre}> Bienvenue dans ShipShotShip ! </Text>
+      <View style={styles.home}>
+        <Text style={styles.titre1}>Bienvenue</Text>
+        <Text style={styles.titre2}>dans</Text>
+        <Text style={styles.titre2}>ShipShotShip !</Text>
 
 
-        <View>
 
+        <View style={{flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',}}>
+         <Image
+            resizeMode = 'contain'
+           style={styles.image}
+           source={{uri: 'https://cdn.megajogos.com.br/images/premium/game-logo/battleship-m_fr_FR.png'}}
+         />
         </View>
 
 
+
         <View style={styles.scanButtonView}>
-         <TouchableOpacity style={styles.scanButton}>
+         <TouchableOpacity style={styles.scanButton} onPress={() => navigate('Game')}>
            <Text style={styles.scanButtonText}>Start Game</Text>
          </TouchableOpacity>
         </View>
 
 
 
-      </ScrollView>
+      </View>
     )
   }
 }
 const styles = StyleSheet.create({
-  titre:{
+  home:{
+    flex: 1,
+    backgroundColor: '#056EB3',
+  },
+
+  titre1:{
     textAlign: 'center',
     fontSize: 20,
     marginLeft: 5,
     marginRight: 5,
-    marginTop:'12%',
-    height: 100,
+    marginTop: '30%',
+    fontSize: 30,
+    height: 50,
     paddingLeft: 5
   },
 
+  titre2:{
+    textAlign: 'center',
+    fontSize: 20,
+    marginLeft: 5,
+    marginRight: 5,
+    fontSize: 30,
+    height: 50,
+    paddingLeft: 5
+  },
+
+  image:{
+    width: 300,
+    height: 300,
+    marginRight: '5%',
+    marginLeft: '5%',
+  },
+
   scanButtonView : {
-  width: '100%',
-  alignItems: 'center',
-  justifyContent: 'center',
-  height: 100,
-  backgroundColor: 'rgba(0,0,0,0.3)',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 100,
 },
 scanButton: {
-  backgroundColor: 'white',
-  width: "90%",
-  borderRadius: 20,
-  height: 70,
-  alignItems: 'center',
-  justifyContent: 'center'
+    backgroundColor: 'white',
+    width: "90%",
+    borderRadius: 20,
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'center'
 },
 
 scanButtonText: {
